560d70ed1acfa3713c36eeddb620fca431a449f9
DenisPopkov06 <pansamrus3@gmail.com>
2026-02-20 20:44:00 +0300

    fix bags

--- src/App.tsx
+++ src/App.tsx
@@ -2,7 +2,6 @@ import Header from "./components/Header/Header";
 import Main from "./components/Main/Main";
 
 function App() {
-
   return (
     <>
       <Header />
--- src/redux/store.tsx
+++ src/redux/store.tsx
@@ -1,22 +1,12 @@
 import { configureStore } from "@reduxjs/toolkit";
 import { setupListeners } from "@reduxjs/toolkit/query";
 import { baseApi } from "../api/baseApi";
-import specializationsFilterReducer from "./slices/specializationsFilterSlice";
-import skillsFilterReducer from "./slices/skillsFilterSlice";
-import ratesFilterReducer from "./slices/ratesFilterSlice";
-import complexitiesFilterReducer from "./slices/complexitiesFilterSlice";
-import searchFilterReducer from "./slices/searchingFilterSlice";
-import paginationReducer from "./slices/paginationSlice";
+import rootReducer from "./rootReducer";
 
 export const store = configureStore({
   reducer: {
     [baseApi.reducerPath]: baseApi.reducer,
-    specializationsFilter: specializationsFilterReducer,
-    skillsFilter: skillsFilterReducer,
-    ratesFilter: ratesFilterReducer,
-    complexitiesFilter: complexitiesFilterReducer,
-    searchingFilter: searchFilterReducer,
-    pagination: paginationReducer,
+    reducer: rootReducer
   },
   middleware: (getDefaultMiddleware) =>
     getDefaultMiddleware().concat(baseApi.middleware),
