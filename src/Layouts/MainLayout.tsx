774ba18a239c056aeedefc166c23789f58697dd4
DenisPopkov06 <pansamrus3@gmail.com>
2026-02-20 22:47:40 +0300

    add routing

--- src/Layouts/MainLayout.tsx
+++ src/Layouts/MainLayout.tsx
@@ -1,12 +1,11 @@
 import Header from "../components/Header/Header";
 import { Outlet } from "react-router-dom"
-import styles from "./styles.module.css"
 
 const MainLayout = () => {
   return (
     <>
       <Header /> 
-      <main className={`${styles.main} container`}>
+      <main className="container">
         <Outlet/>
       </main>
     </>
